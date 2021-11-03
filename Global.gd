2e45fab7ead50bc776cbe4e78fb8ce25690a4f95
ffeke <ffekete84@gmail.com>
2021-11-03 13:58:29 +0000

    player attack, grid based movement

---
+++ Global.gd
@@ -0,0 +1,15 @@
+extends Node
+
+export var grid_size = 16
+
+export var inputs = {
+	"right": Vector2.RIGHT,
+	"left": Vector2.LEFT,
+	"up": Vector2.UP,
+	"down": Vector2.DOWN
+} 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
--- Player.gd
+++ Player.gd
@@ -2,39 +2,91 @@ extends Area2D
 
 signal hit
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-export var speed = 30
+export var speed = 0.75
+
+onready var tween = $Tween
+onready var ray = $RayCast2D
+onready var attack_timer = $AttackTimer
+
+var tile_size = Global.grid_size
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 	$AnimatedSprite.flip_h = false
-	$AnimatedSprite.offset.x = 16
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+	start(Vector2(0,0))
 	$AnimatedSprite.play()
-	var velocity = Vector2()
-	
+	attack_timer.one_shot = true
+
+func _process(delta):
+
+	if(tween.is_active() || !attack_timer.is_stopped()):
+		return
+
 	if(Input.is_action_pressed("ui_up")):
 		$AnimatedSprite.animation = "walk"
-		velocity.y = -1
+		move_with_collision_check(Global.inputs["up"])
+		$AnimatedSprite.flip_v = true
+		$AnimatedSprite.rotation_degrees = 270
+		$AnimatedSprite.flip_h = false
+		
 	elif(Input.is_action_pressed("ui_down")):
-		velocity.y = 1
+		$AnimatedSprite.animation = "walk"
+		move_with_collision_check(Global.inputs["down"])
+		$AnimatedSprite.rotation_degrees =  90
+		$AnimatedSprite.flip_h = false
+		
 	elif(Input.is_action_pressed("ui_left")):
-		velocity.x -= 1
+		$AnimatedSprite.animation = "walk"
+		move_with_collision_check(Global.inputs["left"])
+		$AnimatedSprite.rotation_degrees = 0
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = true
+		
+		
 	elif(Input.is_action_pressed("ui_right")):
-		velocity.x += 1
-	
-	if(velocity.length() > 0):
-			velocity = velocity.normalized() * speed
-			$AnimatedSprite.animation = "walk"
-			$AnimatedSprite.flip_h = velocity.x < 0
-			position += velocity * delta
+		$AnimatedSprite.animation = "walk"
+		move_with_collision_check(Global.inputs["right"])
+		$AnimatedSprite.rotation_degrees = 0
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = false
+		
+		
+	elif(Input.is_action_pressed("ui_fire")):
+		if(attack_timer.is_stopped()):
+			$AnimatedSprite.animation = "attack"
+			attack_timer.start(0.5)
+			print("feuer")
+		
 	else:
-		$AnimatedSprite.animation = "idle"
+		if(attack_timer.is_stopped()):
+			$AnimatedSprite.animation = "idle"
 
+func move_with_collision_check(dir):
+	ray.cast_to = dir * tile_size
+	ray.force_raycast_update()
+	if(!ray.is_colliding()):
+		move(dir)
+
+func move(dir):
+	tween.interpolate_property(self, "position", position, position + dir * tile_size, 
+		speed)
+	tween.start()
+
+func start(pos):
+	position = pos
+	position = position.snapped(Vector2.ONE * tile_size)
+	position += Vector2.ONE * tile_size / 2
+	show()
+	$CollisionShape2D.disabled = false
+
+func _on_Player_body_entered(body):
+	hide()
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+
+
+func _on_AttackTimer_timeout():
+	$AnimatedSprite.animation = "idle"
+	
